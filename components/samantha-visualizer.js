6a089d7d12cd16587e521b02117552ca75e15a16
tausani93 <tausani93@gmail.com>
2023-04-16 01:26:06 +1200

    add to gh

---
+++ components/samantha-visualizer.js
@@ -0,0 +1,170 @@
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+
+const SamanthaVisualizer = ({ scale }) => {
+  const wrapRef = useRef(null);
+  const initializedRef = useRef(false);
+  
+  // https://codepen.io/psyonline/pen/yayYWg
+  useEffect(() => {
+    if (initializedRef.current) {
+      return;
+    }
+    
+    initializedRef.current = true;
+
+    class CustomCurve extends THREE.Curve {
+      constructor() {
+        super();
+      }
+    
+      getPoint(t) {
+        const pi2 = Math.PI * 2;
+        const length = 30;
+        const radius = 5.6;
+    
+        const x = length * Math.sin(pi2 * t);
+        const y = radius * Math.cos(pi2 * 3 * t);
+        let z, p;
+    
+        p = t % 0.25 / 0.25;
+        p = t % 0.25 - (2 * (1 - p) * p * -0.0185 + p * p * 0.25);
+        if (Math.floor(t / 0.25) === 0 || Math.floor(t / 0.25) === 2) {
+          p *= -1;
+        }
+        z = radius * Math.sin(pi2 * 2 * (t - p));
+    
+        return new THREE.Vector3(x, y, z);
+      }
+    }
+
+    const wrap = wrapRef.current;
+  
+    const areaWidth = window.innerWidth;
+    const areaHeight = window.innerHeight;
+
+    const canvasSize = Math.min(areaWidth, areaHeight) * scale;
+  
+    const length = 30;
+    const radius = 5.6;
+  
+    const rotatevalue = 0.035;
+    let acceleration = 0;
+    let animatestep = 0;
+    let toend = false;
+  
+    const pi2 = Math.PI * 2;
+  
+    const group = new THREE.Group();
+    let mesh, ringcover, ring;
+  
+    const camera = new THREE.PerspectiveCamera((60 * scale), 1, 1, 10000);
+    camera.position.z = 150;
+  
+    const scene = new THREE.Scene();
+    scene.add(group);
+  
+    mesh = new THREE.Mesh(
+      new THREE.TubeGeometry(
+        new CustomCurve(),
+        200,
+        1.1,
+        2,
+        true
+      ),
+      new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+      })
+    );
+    group.add(mesh);
+  
+    ringcover = new THREE.Mesh(
+      new THREE.PlaneGeometry(50, 15, 1),
+      new THREE.MeshBasicMaterial({ color: 0xdc5037, opacity: 0, transparent: true })
+    );
+    ringcover.position.x = length + 1;
+    ringcover.rotation.y = Math.PI / 2;
+    group.add(ringcover);
+  
+    ring = new THREE.Mesh(
+      new THREE.RingGeometry(4.3, 5.55, 32),
+      new THREE.MeshBasicMaterial({ color: 0xffffff, opacity: 0, transparent: true })
+    );
+    ring.position.x = length + 1.1;
+    ring.rotation.y = Math.PI / 2;
+    group.add(ring);
+  
+    const renderer = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(canvasSize, canvasSize);
+    renderer.setClearColor("#DC5037");
+  
+    wrap.appendChild(renderer.domElement);
+  
+    document.body.addEventListener("mousedown", start, false);
+    document.body.addEventListener("touchstart", start, false);
+    document.body.addEventListener("mouseup", back, false);
+    document.body.addEventListener("touchend", back, false);
+  
+    animate();
+
+    function start() {
+      toend = true;
+    }
+  
+    function back() {
+      toend = false;
+    }
+  
+    function render() {
+      let progress;
+  
+      animatestep = Math.max(0, Math.min(240, toend ? animatestep + 1 : animatestep - 4));
+      acceleration = easing(animatestep, 0, 1, 240);
+  
+      if (acceleration > 0.35) {
+        progress = (acceleration - 0.35) / 0.65;
+        group.rotation.y = (-Math.PI / 2) * progress;
+        group.position.z = 50 * progress;
+        progress = Math.max(0, (acceleration - 0.97) / 0.03);
+        mesh.material.opacity = 1 - progress;
+        ringcover.material.opacity = ring.material.opacity = progress;
+        ring.scale.x = ring.scale.y = 0.9 + 0.1 * progress;
+      }
+  
+      renderer.render(scene, camera);
+    }
+  
+    function animate() {
+      mesh.rotation.x += rotatevalue + acceleration;
+      render();
+      requestAnimationFrame(animate);
+    }
+  
+    function easing(t, b, c, d) {
+      if ((t /= d / 2) < 1) return (c / 2) * t * t + b;
+      return (c / 2) * ((t -= 2) * t * t + 2) + b;
+    }
+  }, [scale]);
+
+  return (
+    <div
+      style={{
+        overflow: 'hidden',
+        userSelect: 'none',
+        WebkitUserSelect: 'none',
+        MozUserSelect: 'none',
+        msUserSelect: 'none',
+        backgroundColor: '#DC5037',
+        WebkitTouchCallout: 'none',
+        WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+      }}
+    >
+      <div ref={wrapRef} id="wrap"/>
+    </div>
+  );
+};
+
+export default React.memo(SamanthaVisualizer);
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,11 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: false,
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      backgroundColor: {
+        'custom-orange': '#DC5037',
       },
     },
   },
